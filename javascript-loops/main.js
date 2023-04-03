54e5fa5ff9200669101b6bdc23e29a227af0075f
Kitlong Lai <thekitlong@gmail.com>
2023-04-03 15:58:40 +0000

    Resubmission javascript-loops.

--- javascript-loops/main.js
+++ javascript-loops/main.js
@@ -9,7 +9,7 @@
 function getNumbersToTen() {
   const numbers = [];
   let currentNumber = 1;
-  while (currentNumber < 10) {
+  while (currentNumber <= 10) {
     numbers.push(currentNumber);
     currentNumber++;
   }
@@ -21,7 +21,7 @@ console.log('getNumbersToTen: ', getNumbersToTen());
 function getEvenNumbersToTwenty() {
   const evenNumbers = [];
   let currentNumber = 2;
-  while (currentNumber < 20) {
+  while (currentNumber <= 20) {
     evenNumbers.push(currentNumber);
     currentNumber += 2;
   }
