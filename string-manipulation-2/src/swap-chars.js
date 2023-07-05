ba9f72229f43073476b9d477f71bdae3aa4b38b9
Kitlong Lai <thekitlong@gmail.com>
2023-07-05 05:21:03 +0000

    completed string-manipulation-2

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,6 +1,14 @@
 /* exported swapChars */
 function swapChars(firstIndex, secondIndex, string) {
-  const array = string.split('');
-  swapChars(firstIndex, secondIndex, array);
-  return array.join('');
+  var swap = '';
+  for (var i = 0; i < string.length; i++) {
+    if (i === firstIndex) {
+      swap += string[secondIndex];
+    } else if (i === secondIndex) {
+      swap += string[firstIndex];
+    } else {
+      swap += string[i];
+    }
+  }
+  return swap;
 }
